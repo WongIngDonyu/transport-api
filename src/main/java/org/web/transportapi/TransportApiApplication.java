8560a79d5fc29c0371a099374a5606238086f551
WongIngDonyu <wongingdonyu@gmail.com>
2024-12-14 18:34:17 +0300

    adminka

--- src/main/java/org/web/transportapi/TransportApiApplication.java
+++ src/main/java/org/web/transportapi/TransportApiApplication.java
@@ -2,11 +2,21 @@ package org.web.transportapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TransportApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TransportApiApplication.class, args);
+    }
+}
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Application deployed successfully via Jenkins!";
     }
 }
